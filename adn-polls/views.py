30470f7492eb46a559d8dd7002cc29cd25fdd193
Abraham Williams <4braham@gmail.com>
2013-10-28 01:49:28 -0700

    moved star and repost actions to templates, reworte the voteing template section

--- adn-polls/views.py
+++ adn-polls/views.py
@@ -24,3 +24,25 @@ class Actions(UIModule):
         }
         path = 'templates/actions_{}.html'.format(action['type'])
         return self.render_string(path, **context)
+
+
+class PollsStar(UIModule):
+
+    def render(self, poll_id, user_id, starred_by):
+        '''Render a star to star a post'''
+        context = {
+            'poll_id': poll_id,
+            'is_starred': user_id in starred_by,
+        }
+        return self.render_string('templates/polls_star.html', **context)
+
+
+class PollsRepost(UIModule):
+
+    def render(self, poll_id, user_id, reposted_by):
+        '''Render a repost a poll icon'''
+        context = {
+            'poll_id': poll_id,
+            'is_reposted': user_id in reposted_by,
+        }
+        return self.render_string('templates/polls_repost.html', **context)
