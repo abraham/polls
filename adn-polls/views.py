679fee8bfd630bfad0fed48866a82ef323cad421
Abraham Williams <4braham@gmail.com>
2013-10-28 00:18:19 -0700

    moved actions to their own template files

--- adn-polls/views.py
+++ adn-polls/views.py
@@ -13,3 +13,14 @@ class NavbarUser(UIModule):
                 'current_user': current_user,
             }
             return self.render_string('templates/navbar_user.html', **context)
+
+
+class Actions(UIModule):
+
+    def render(self, action):
+        '''Render actions from with templates'''
+        context = {
+            'action': action,
+        }
+        path = 'templates/actions_{}.html'.format(action['type'])
+        return self.render_string(path, **context)
