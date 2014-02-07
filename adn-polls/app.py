945cb673881faad0e28ab311865d5dee35fec767
Abraham Williams <4braham@gmail.com>
2014-02-06 20:26:32 -0800

    updated user profile icons

--- adn-polls/app.py
+++ adn-polls/app.py
@@ -384,8 +384,8 @@ class UsersIdHandler(BaseHandler):
         recent_actions = actions.find_recent_by_user_id(db=db, user_id=ObjectId(user_id))
         for action in recent_actions:
             action['created_at_human'] = momentpy.from_now(action['created_at'], from_utc=True)
-        subtitle = u'<a href="https://alpha.app.net/{}" target="_blank" title="View on ADN" rel="me"><span class="glyphicon glyphicon-link"></span></a> '.format(viewed_user['user_name'])
-        subtitle += u'<a href="https://omega.app.net/new-message?to={}" target="_blank" title="Message on Omega"><span class="glyphicon glyphicon-envelope"></span></a> '.format(viewed_user['adn_id'])
+        subtitle = u'<a href="https://alpha.app.net/{}" class="external" title="View on ADN" rel="me"><span class="glyphicon glyphicon-new-window"></span></a> '.format(viewed_user['user_name'])
+        subtitle += u'<a href="https://omega.app.net/new-message?to={}" class="external" title="Message on Omega"><span class="glyphicon glyphicon-envelope"></span></a> '.format(viewed_user['adn_id'])
         subtitle += u'{} polls &middot; '.format(viewed_user['polls_count'])
         subtitle += u'{} votes &middot; '.format(viewed_user['votes_count'])
         subtitle += u'{} views'.format(viewed_user['views'])
