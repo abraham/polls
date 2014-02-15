298b5cf01378a80fcd1e68670744bb610eabb1c8
Abraham Williams <4braham@gmail.com>
2014-02-15 15:23:49 -0800

    Added decorator for requiring user in path

--- adn-polls/decorators.py
+++ adn-polls/decorators.py
@@ -26,7 +26,7 @@ def require_poll(f):
 
     @wraps(f)
     def wrapper(self, *args, **kwargs):
-        db = self.db
+        db      = self.db
         poll_id = object_id(args[0])
 
         if poll_id is None:
@@ -39,8 +39,34 @@ def require_poll(f):
             self.write_error(404)
             return
 
-        self.current_poll = poll
-        self.current_poll_id = poll_id
+        self.current_poll       = poll
+        self.viewed_poll        = poll
+        self.current_poll_id    = poll_id
+        self.viewed_poll_id     = poll_id
+        f(self, *args, **kwargs)
+
+    return wrapper
+
+
+def require_user(f):
+
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        db      = self.db
+        user_id = object_id(args[0])
+
+        if user_id is None:
+            self.write_error(404)
+            return
+
+        user = users.find_by_id(db=db, user_id=user_id)
+
+        if user is None:
+            self.write_error(404)
+            return
+
+        self.viewed_user    = user
+        self.viewed_user_id = user_id
         f(self, *args, **kwargs)
 
     return wrapper
